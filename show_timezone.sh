c6b02aaa7561b8a707b25f5572b44a082aabbf85
GANitak <43230341+GANitak@users.noreply.github.com>
2024-10-06 17:06:46 +0200

    show_timezone.sh and countup clock
    Display timezone with TZ alias, and a clock counting up with CU alias respectively

---
+++ show_timezone.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Define ANSI color escape codes
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color/reset
+
+echo -e "${GREEN}Current time in ${ORANGE}UTC+9 ${RED}(Melbourne):${NC}"
+TZ="Melbourne/VIC" date
+echo -e "${GREEN}Current time in ${ORANGE}UTC-6 ${RED}(Central Time):${NC}"
+TZ="America/Chicago" date
+echo ############## 
+echo -e "${GREEN}Current time in ${ORANGE}UTC-7 ${RED}(Mountain Time):${NC}"
+TZ="America/Denver" date
+echo ##############
+echo -e "${GREEN}Current ${RED}time ${GREEN}in UTC-8 ${RED}(Pacific Time):${NC}"
+TZ="America/Los_Angeles" date
+echo ##############
+# Create a line break
+echo
+
+# Display current time in UTC-5 (Eastern Time)
+echo -e "${GREEN}Current time in ${ORANGE}UTC-5 (Eastern Time):${NC}"
+TZ="America/New_York" date
+echo -e "${GREEN}Current time in ${ORANGE}UTC-9 ${RED}(Alaska Time):${NC}"
+TZ="America/Anchorage" date
+echo ##############
+echo -e "${GREEN}Current time in ${ORANGE}UTC-10 ${RED}(Hawaii-Aleutian Standard Time):${NC}"
+TZ="Pacific/Honolulu" date
